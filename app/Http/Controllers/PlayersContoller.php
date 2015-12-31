1db74e746927422ca53d73fe6181ab07264488ac
Richard Ellis <info@lookstechnical.co.uk>
2015-12-31 19:08:36 +0000

    up
    Signed-off-by: Richard Ellis <info@lookstechnical.co.uk>

--- app/Http/Controllers/PlayersContoller.php
+++ app/Http/Controllers/PlayersContoller.php
@@ -25,20 +25,20 @@ class PlayersController extends Controller
 	
 	public function store(Request $request)
 	{
-		var_dump($request->get('club_id'));die();
-		$club = Club::where('_id',$request->get('club_id'))->first();
+		var_dump($request->club_id));die();
+		$club = Club::where('_id',$request->club_id)->first();
 		
 		
 		
-			$team = $club->teams()->where('_id','team_id')->first();
+			$team = $club->teams()->where('_id',$request->team_id)->first();
 		
-			$user = User::where('email',$request->get('email'))->first();
+			$user = User::where('email',$request->email)->first();
 			if(empty($user)){
 				$user = new User();
-				$user->first_name = $request->get('first_name');
-				$user->last_name = $request->get('last_name');
-				$user->email = $request->get('email');
-				$user->position = $request->get('position');
+				$user->first_name = $request->first_name;
+				$user->last_name = $request->last_name;
+				$user->email = $request->email;
+				$user->position = $request->position;
 				$user->active = false;
 				$user->save();
 			}
