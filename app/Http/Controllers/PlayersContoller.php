502366c2f4896428bca39a9ff2829a18b48b1d7c
Richard Ellis <info@lookstechnical.co.uk>
2015-12-31 19:11:39 +0000

    up
    Signed-off-by: Richard Ellis <info@lookstechnical.co.uk>

--- app/Http/Controllers/PlayersContoller.php
+++ app/Http/Controllers/PlayersContoller.php
@@ -18,6 +18,7 @@ use App\User;
 use App\Models\Team;
 use App\Models\Club;
 use App\Models\User\Role;
+use Input;
 
 class PlayersController extends Controller 
 {
@@ -25,20 +26,20 @@ class PlayersController extends Controller
 	
 	public function store(Request $request)
 	{
-		var_dump($request->input('club_id'));die();
-		$club = Club::where('_id',$request->input('club_id'))->first();
+		var_dump(Input::get('club_id'));die();
+		$club = Club::where('_id',Input::get('club_id'))->first();
 		
 		
 		
-			$team = $club->teams()->where('_id',$request->input('team_id'))->first();
+			$team = $club->teams()->where('_id',Input::get('team_id'))->first();
 		
-			$user = User::where('email',$request->input('email'))->first();
+			$user = User::where('email',Input::get('email'))->first();
 			if(empty($user)){
 				$user = new User();
-				$user->first_name = $request->input('first_name');
-				$user->last_name = $request->input('last_name');
-				$user->email = $request->input('email');
-				$user->position = $request->input('position');
+				$user->first_name = Input::get('first_name');
+				$user->last_name = Input::get('last_name');
+				$user->email = Input::get('email');
+				$user->position = Input::get('position');
 				$user->active = false;
 				$user->save();
 			}
